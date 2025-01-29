a3723c51f8bf1c78a80fd0806ea98d5a41ca4a2e
Akyl <akylbek.aws@gmail.com>
2025-01-29 14:52:00 -0500

    3

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -13,10 +13,10 @@ resource "aws_db_subnet_group" "dev_proje_1_db_subnet_group" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage       = 10
-  storage_type            = "gp2"
+  allocated_storage       = 20
+  storage_type            = "gp3"
   engine                  = "mysql"
-  engine_version          = "5.7"
+  engine_version          = "8.0.40"
   instance_class          = "db.m5d.large"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
